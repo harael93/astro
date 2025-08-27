e5e2085e1020250f794369b29f07253c139df46a
harael93 <liquidsoliddesign@gmail.com>
2025-08-27 09:20:58 -0400

    server on 8080 set for dev

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Dict, Any
 from skyfield.api import load, Topos
@@ -8,7 +9,15 @@ from astral import moon
 import datetime
 
 
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"] ,
+    allow_headers=["*"]
+)
 
 class ChartRequest(BaseModel):
     birth_date: str
